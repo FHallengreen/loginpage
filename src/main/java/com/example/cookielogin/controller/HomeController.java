0ef0b27fdd5c75c7b849de86e8622dc09e10e18e
FHallengreen <fred70p9@stud.kea.dk>
2022-11-15 14:01:06 +0100

    added logout button

--- src/main/java/com/example/cookielogin/controller/HomeController.java
+++ src/main/java/com/example/cookielogin/controller/HomeController.java
@@ -23,31 +23,36 @@ public class HomeController {
 
     @PostMapping("/login")
     public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession httpSession) {
-        User user = new User(email,password);
+        User user = new User(email, password);
         User user2 = userRepository.findUserByEmail(email);
 
         if (user.getEmail().equals(user2.getEmail()) && user.getPassword().equals(user2.getPassword())) {
             httpSession.setAttribute("user", email);
             model.addAttribute("username", email);
             return "redirect:/welcome";
-        }
-        else return "redirect:/error";
+        } else return "redirect:/error";
     }
 
     @GetMapping("/welcome")
-    public String welcomeUser(HttpSession httpSession, Model model){
+    public String welcomeUser(HttpSession httpSession, Model model) {
 
-        model.addAttribute("username",userRepository.findUserByEmail((String) httpSession.getAttribute("user")).getEmail());
-    httpSession.getAttribute("user");
+        model.addAttribute("username", userRepository.findUserByEmail((String) httpSession.getAttribute("user")).getEmail());
+        httpSession.getAttribute("user");
 
-    return "welcome";
+        return "welcome";
     }
 
     @GetMapping("/page-two")
     public String pageTwo(HttpSession httpSession) {
         httpSession.getAttribute("user");
-            return "page-two";
-        }
+        return "page-two";
+    }
 
+    @GetMapping("/cookieinvalidate")
+    public String invalidateCookie(HttpSession session){
+        System.out.println(session.getAttribute("username"));
+        session.invalidate();
+        return "redirect:/";
+    }
 
 }
